57bf80ea99c94c5f98acbc1d8ce4f809b69d6fcb
Mamdouh-Radwan <engmmmradwan@gmail.com>
2024-06-29 22:16:12 +0300

    First Commit

---
+++ js/index.js
@@ -0,0 +1,116 @@
+let cityInput = document.getElementById('cityInput');
+
+cityInput.addEventListener('keyup', function(e){
+   getWeather(`q=${e.target.value}`);
+});
+
+async function getWeather(city){
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=a71bde5aeb844e9fbb4110647242406&${city}&days=3`);
+    let data = await response.json();
+    console.log(data);
+    weatherObject = dataExtract(data);
+    console.log(weatherObject);
+    displayWeather(weatherObject);
+    
+}
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function dataExtract(dObject){
+    let curDate = new Date(dObject.location.localtime.split(" ")[0]);
+    
+    let foreDate1 = new Date (dObject.forecast.forecastday[1].date)
+
+    let foreDate2 = new Date (dObject.forecast.forecastday[2].date)
+    
+    weatherData = {
+        cityName: dObject.location.name,
+        currentDay: {
+            dateDay: days[curDate.getDay()],
+            dateMonth: months[curDate.getMonth()],
+            date: curDate.getDate(),
+            temp: dObject.current.temp_c,
+            icon: dObject.current.condition.icon,
+            condition: dObject.current.condition.text,
+            rain: dObject.current.precip_in,
+            windSpeed: dObject.current.wind_kph,
+            windDir: dObject.current.wind_dir
+        },
+        forecastday1: {
+            dateDay: days[foreDate1.getDay()],
+            icon: dObject.forecast.forecastday[1].day.condition.icon,
+            maxTemp: dObject.forecast.forecastday[1].day.maxtemp_c,
+            minTemp: dObject.forecast.forecastday[1].day.mintemp_c,
+            condition: dObject.forecast.forecastday[1].day.condition.text
+        },
+        forecastday2: {
+            dateDay: days[foreDate2.getDay()],
+            icon: dObject.forecast.forecastday[2].day.condition.icon,
+            maxTemp: dObject.forecast.forecastday[2].day.maxtemp_c,
+            minTemp: dObject.forecast.forecastday[2].day.mintemp_c,
+            condition: dObject.forecast.forecastday[2].day.condition.text
+        }
+    }
+    return weatherData;
+}
+
+function displayWeather(dataObject) {
+    let displayScreen = document.getElementById('weatherScreen');
+
+    displayScreen.innerHTML = `
+    <div class="row">
+          <div class="temp-card col-lg-4 text-white text-center px-0">
+            <div class="date d-flex justify-content-between align-items-center px-2">
+              <p>${dataObject.currentDay.dateDay}</p>
+              <p>${dataObject.currentDay.date}${dataObject.currentDay.dateMonth}</p>
+            </div>
+            <div class="weather p-3">
+              <p class="city-name text-start">${dataObject.cityName}</p>
+              <div class="temprature d-flex align-items-center justify-content-between">
+                <p class="degree display-1 fw-bolder">${dataObject.currentDay.temp}°C</p>
+                <img src="https:${dataObject.currentDay.icon}" alt="">
+              </div>
+              <p class="condition text-start text-info">${dataObject.currentDay.condition}</p>
+              <div class="info d-flex align-items-center justify-content-center">
+                <div class="rain me-5">
+                  <img class="me-1" src="./images/icon-umberella.png" alt="">
+                  <span>${dataObject.currentDay.rain}%</span>
+                </div>
+                <div class="wind me-5">
+                  <img class="me-1" src="./images/icon-wind.png" alt="">
+                  <span>${dataObject.currentDay.windSpeed}km/h</span>
+                </div>
+                <div class="wind-dir me-5">
+                  <img class="me-1" src="./images/icon-compass.png" alt="">
+                  <span>${dataObject.currentDay.windDir}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tmrw-temp-card col-lg-4 text-white text-center px-0">
+            <div class="tmrw date text-center">
+              <p>${dataObject.forecastday1.dateDay}</p>
+            </div>
+            <div class="weather p-3 text-center">
+              <img class="py-3" src="https:${dataObject.forecastday1.icon}" alt="">
+              <p class="fs-2 fw-bold m-0">${dataObject.forecastday1.maxTemp}°C</p>
+              <p class="text-secondary">${dataObject.forecastday1.minTemp}°C</p>
+              <p class="condition text-info mt-5">Sunny</p>
+            </div>
+          </div>
+          <div class="temp-card col-lg-4 text-white text-center px-0">
+            <div class="date text-center">
+              <p>${dataObject.forecastday2.dateDay}</p>
+            </div>
+            <div class="weather p-3 text-center">
+              <img class="py-3" src="https:${dataObject.forecastday2.icon}" alt="">
+              <p class="fs-2 fw-bold m-0">${dataObject.forecastday2.maxTemp}°C</p>
+              <p class="text-secondary">${dataObject.forecastday2.minTemp}°C</p>
+              <p class="condition text-info mt-5">Sunny</p>
+            </div>
+          </div>
+        </div>
+    `
+}
+getWeather('q=auto:ip');
